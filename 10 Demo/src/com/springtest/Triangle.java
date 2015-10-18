a173ff3410236abfb7cab7149f68e9b072387d64
unknown <b_tootoonchi@yahoo.com>
2015-10-18 18:32:50 -0400

    ApplicationContextAware

--- 10 Demo/src/com/springtest/Triangle.java
+++ 10 Demo/src/com/springtest/Triangle.java
@@ -1,13 +1,21 @@
 package com.springtest;
 
-import java.util.List;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
-public class Triangle {
+public class Triangle implements ApplicationContextAware, BeanNameAware {
 	private String type;
 	private String price;
 	private int height;
 	
 	private Point pointA;
+	private Point pointB;
+	private Point pointC;
+	
+	private ApplicationContext context;
+	
 	public Point getPointA() {
 		return pointA;
 	}
@@ -32,10 +40,6 @@ public class Triangle {
 		this.pointC = pointC;
 	}
 
-	private Point pointB;
-	private Point pointC;
-
-
 	public Triangle(String price) {
 		this.price = price;
 	}
@@ -58,5 +62,15 @@ public class Triangle {
 		System.out.println("Point A: (x, y) = (" + pointA.getX() + ", " + pointA.getY() + ")");
 		System.out.println("Point B: (x, y) = (" + pointB.getX() + ", " + pointB.getY() + ")");
 		System.out.println("Point C: (x, y) = (" + pointC.getX() + ", " + pointC.getY() + ")");
+	}
+
+	@Override
+	public void setBeanName(String beanName) {
+		System.out.println("Bean name is " + beanName);
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext context) throws BeansException {
+		this.context = context;
 	}
 }
