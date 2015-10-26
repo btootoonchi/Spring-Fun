797602e6b4f1dc28b40ab00671678d95864b41f3
unknown <b_tootoonchi@yahoo.com>
2015-10-26 10:51:28 -0400

    JSR-250 Annotations: Resource, PostConstruct, and PreDestroy

--- 10 Demo/src/com/springtest/Circle.java
+++ 10 Demo/src/com/springtest/Circle.java
@@ -1,8 +1,8 @@
 package com.springtest;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-//import org.springframework.beans.factory.annotation.Required;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
 
 public class Circle implements Shape {
 	private Point center;
@@ -11,9 +11,7 @@ public class Circle implements Shape {
 		return center;
 	}
 
-//	@Required
-	@Qualifier("circleRelated")
-	@Autowired
+	@Resource(name="pointA")
 	public void setCenter(Point center) {
 		this.center = center;
 	}
@@ -21,5 +19,14 @@ public class Circle implements Shape {
 	@Override
 	public void draw() {
 		System.out.println("Drawing a circle. center is (" + center.getX() + ", " + center.getY() +")");
+	}
+	
+	@PostConstruct
+	public void init(){
+		System.out.println("Init of Circle.");
+	}
+	@PreDestroy
+	public void destroy(){
+		System.out.println("Destroy of Circle.");
 	}
 }
