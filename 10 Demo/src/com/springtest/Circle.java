ce2433aead207e5157c4220dd84a8f0337f1aff8
unknown <b_tootoonchi@yahoo.com>
2015-10-30 10:17:56 -0400

    Event Handling

--- 10 Demo/src/com/springtest/Circle.java
+++ 10 Demo/src/com/springtest/Circle.java
@@ -5,13 +5,16 @@ import javax.annotation.PreDestroy;
 import javax.annotation.Resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class Circle implements Shape {
+public class Circle implements Shape, ApplicationEventPublisherAware {
 	private Point center;
-
+	private ApplicationEventPublisher publisher;
+	
 	@Autowired
 	private MessageSource messageSource; 
 	
@@ -37,6 +40,9 @@ public class Circle implements Shape {
 		System.out.println(this.messageSource.getMessage("Drawing.Circle", null, "Default Drawing Circle Message", null) +
 				this.messageSource.getMessage("Drawing.Point", new Object[] {center.getX(), center.getY()}, "Default Point Circle Message", null)
 				);
+		
+		DrawEvent drawEvent = new DrawEvent(this);
+		publisher.publishEvent(drawEvent);
 	}
 	
 	@PostConstruct
@@ -46,5 +52,10 @@ public class Circle implements Shape {
 	@PreDestroy
 	public void destroy(){
 		System.out.println("Destroy of Circle.");
+	}
+
+	@Override
+	public void setApplicationEventPublisher(ApplicationEventPublisher publisher) {
+		this.publisher = publisher;
 	}
 }
