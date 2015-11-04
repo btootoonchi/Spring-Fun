b44744575e8548ccd013173c1034647b847db04e
unknown <b_tootoonchi@yahoo.com>
2015-11-04 14:14:36 -0500

    implemented the Logging aspect

---
+++ 11 DemoAOP/src/com/springtest/aspect/LoggingAspect.java
@@ -0,0 +1,17 @@
+package com.springtest.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Aspect
+public class LoggingAspect {
+	
+	/** 
+	 * This is the method which I would like to execute
+	 * before a selected method execution.
+	*/
+	@Before("execution(public String getName())")
+	public void loggingAdvice() {
+		System.out.println("Advicr run. Get Method called.");
+	}
+}
