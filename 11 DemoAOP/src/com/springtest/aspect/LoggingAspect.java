0f14a34419def58ecc6a7be196900e93bdd84962
unknown <b_tootoonchi@yahoo.com>
2015-11-05 09:09:57 -0500

    using Pointcuts Expressions

--- 11 DemoAOP/src/com/springtest/aspect/LoggingAspect.java
+++ 11 DemoAOP/src/com/springtest/aspect/LoggingAspect.java
@@ -2,6 +2,7 @@ package com.springtest.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
@@ -10,8 +11,16 @@ public class LoggingAspect {
 	 * This is the method which I would like to execute
 	 * before a selected method execution.
 	*/
-	@Before("execution(public * get*())")
+	@Before("allGetters()")
 	public void loggingAdvice() {
-		System.out.println("Advicr run. Get Method called.");
+		System.out.println("Advice run. Get Method called.");
 	}
+	
+	@Before("allGetters()")
+	public void gettingAdvice() {
+		System.out.println("Getting Advice run. Get Method called.");
+	}
+	
+	@Pointcut("execution(public * get*())")
+	public void allGetters() {}
 }
