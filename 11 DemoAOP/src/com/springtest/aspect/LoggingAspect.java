2c5d80b7f6a531794b827df564ea56209806e3fe
unknown <b_tootoonchi@yahoo.com>
2015-11-05 09:05:17 -0500

    using Wildcard Expressions

--- 11 DemoAOP/src/com/springtest/aspect/LoggingAspect.java
+++ 11 DemoAOP/src/com/springtest/aspect/LoggingAspect.java
@@ -10,7 +10,7 @@ public class LoggingAspect {
 	 * This is the method which I would like to execute
 	 * before a selected method execution.
 	*/
-	@Before("execution(public String com.springtest.model.Circle.getName())")
+	@Before("execution(public * get*())")
 	public void loggingAdvice() {
 		System.out.println("Advicr run. Get Method called.");
 	}
