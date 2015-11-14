e7337d4f02a138c6dbeed4acb03b5403afcb6722
unknown <b_tootoonchi@yahoo.com>
2015-11-14 13:51:31 -0500

    used AOP xml configuration

--- 11 DemoAOP/src/com/springtest/aspect/LoggingAspect.java
+++ 11 DemoAOP/src/com/springtest/aspect/LoggingAspect.java
@@ -12,56 +12,56 @@ import org.aspectj.lang.annotation.Pointcut;
 
 import com.springtest.model.Circle;
 
-@Aspect
+//@Aspect // go to xml file, see <aop:config> <aop:aspect>
 public class LoggingAspect {
 	
 	/** 
 	 * This is the method which I would like to execute
 	 * before a selected method execution.
 	*/
-	@Before("allCircleMethods()")
+	/*@Before("allCircleMethods()")
 	public void loggingAdvice(JoinPoint joinPoint) {
 		System.out.println("Advice run. Get Method called.");
 		System.out.println(joinPoint.getTarget());
 		
 		Circle circle = (Circle) joinPoint.getTarget();
-	}
+	}*/
 	
-	@Before("args(name)")
+	/*@Before("args(name)")
 	public void stringArgumentsMethod(String name) {
-	/*@Before("args(String)")
-	public void stringArgumentsMethod() {
-		System.out.println("A method that takes String arguments has been called.");*/
+//	@Before("args(String)")
+//	public void stringArgumentsMethod() {
+//		System.out.println("A method that takes String arguments has been called.");
 		System.out.println("A method that takes String arguments has been called. The name is " + name);
-	}
+	}*/
 	
-	@After("args(name)")
+	/*@After("args(name)")
 	@AfterReturning("args(name)")
 	@AfterThrowing("args(name)")
 	public void afterAdviceMethod(String name) {
 		System.out.println("After annotation executed. The name is "+ name);
-	}
+	}*/
 	
 	/*@AfterReturning(pointcut="args(name)", returning="retrunString")
 	public void afterReturnningAdvic(String name, String returnString) {
 		System.out.println("The after returnnig advic runs and name is "+name+". The return string is "+returnString);
 	}*/
 	
-	@Before("allGetters()")
+	/*@Before("allGetters()")
 	public void gettingAdvice() {
 		System.out.println("Getting Advice run. Get Method called.");
-	}
+	}*/
 	
-	@Pointcut("execution(public * get*())")
-	public void allGetters() {}
+	/*@Pointcut("execution(public * get*())")
+	public void allGetters() {}*/ //go to xml file, see <aop:pointcut>
 	
 //	@Pointcut("execution(* * com.springtest.model.Circle.*(..))")
-	@Pointcut("within(com.springtest.model.Circle)") // for all methods
+//	@Pointcut("within(com.springtest.model.Circle)") // for all methods
 //	@Pointcut("within(com.springtest.model.*)") // for all classes 
 //	@Pointcut("within(com.springtest.model..)") //also for sub packages
-	public void allCircleMethods() {}
+//	public void allCircleMethods() {}
 	
-	@Around("allGetters()")
+//	@Around("allGetters()") //go to xml file, see <aop:around>
 	public Object aroundAdvice(ProceedingJoinPoint preceedingJoinPoint) {
 		Object object = null;
 		try {
